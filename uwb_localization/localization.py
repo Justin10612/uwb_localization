cfe7134514a8a93f9295b237f51565c4674a6b28
Justin10612 <atom.9031@gmail.com>
2024-04-30 15:02:49 +0800

    update

--- uwb_localization/localization.py
+++ uwb_localization/localization.py
@@ -10,7 +10,7 @@ from geometry_msgs.msg import Vector3
 
 class UwbLocalization(Node):
     # Constants
-    kDataCount = 1
+    kDataCount = 3
     # Dis
     r0 = []
     r1 = []
@@ -19,82 +19,109 @@ class UwbLocalization(Node):
     ty = []
     # Anchor Coordinate
     # [x,y,z]
-    anchor0 = np.array([0, 0, 60])
-    anchor1 = np.array([16, 56, 60])
-    anchor2 = np.array([-16, 56, 60])
+    anchor = np.array([[0, 0, 22],
+                      [16, 56, 2],
+                      [-16, 56, -2]])
+    anchor0 = np.array([0, 0, 22])
+    anchor1 = np.array([16, 56, 2])
+    anchor2 = np.array([-16, 56, -2])
     # initail guess
-    last_guess = np.array([100, 100, 60])
+    initial_guess = np.array([1, 1, 1])
 
     def __init__(self):
-        super().__init__('d_localization')
-        # Subscriber
-        self.dis_sub_ = self.create_subscription(Vector3, 'uwb_distance', self.dis_callback, 10)
-        self.dis_sub_
-        # Publisher
-        self.pose_pub_ = self.create_publisher(Vector3, 'human_pose', 10)
+      super().__init__('d_localization')
+      # Subscriber
+      self.dis_sub_ = self.create_subscription(Vector3, 'uwb_distance', self.dis_callback, 10)
+      self.dis_sub_
+      # Publisher
+      self.pose_pub_ = self.create_publisher(Vector3, 'human_pose', 10)
 
     # main Loop    
     def dis_callback(self, msgs):
-        # self.get_logger().info("Start_localization")
-        try:
-            # Make sure there are ten dis_data.
-            if len(self.r0)<self.kDataCount or len(self.r1)<self.kDataCount or len(self.r2)<self.kDataCount:
-                # meter to center meter
-                self.r0.append(msgs.x*100)
-                self.r1.append(msgs.y*100)
-                self.r2.append(msgs.z*100)
-                # print(len(self.r0))
-            else:
-                # Average the distance
-                d0 = np.mean(self.r0)
-                d1 = np.mean(self.r1)
-                d2 = np.mean(self.r2)
-                # draw the Anchor Position
-                map_image = np.zeros((720, 720, 3), np.uint8)
-                # map_image[:] = (128, 128, 128)
-                cv2.putText(map_image, '0= '+str(d0), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-                cv2.putText(map_image, '1= '+str(d1), (100, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-                cv2.putText(map_image, '2= '+str(d2), (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-                cv2.circle(map_image, (360+self.anchor0[0], 360+self.anchor0[1]), 2, (255,255,255), 0)
-                cv2.circle(map_image, (360-self.anchor1[0], 360+self.anchor1[1]), 2, (255,255,255), 0)
-                cv2.circle(map_image, (360-self.anchor2[0], 360+self.anchor2[1]), 2, (255,255,255), 0)
-                # Define Equation
-                def equations(p):
-                    x, y, z = p
-                    eq1 = (x - self.anchor0[0])**2 + (y - self.anchor0[1])**2 + (z - self.anchor0[2])**2 - d0**2
-                    eq2 = (x - self.anchor1[0])**2 + (y - self.anchor1[1])**2 + (z - self.anchor1[2])**2 - d1**2
-                    eq3 = (x - self.anchor2[0])**2 + (y - self.anchor2[1])**2 + (z - self.anchor2[2])**2 - d2**2
-                    return [eq1, eq2, eq3]
-                # 初始猜測值
-                initial_guess = np.array([100, 100, 60])
-                # 使用 fsolve 求解
-                result = fsolve(equations, initial_guess)
-                # Result
-                tag_pos = np.array([int(round(float(result[0]), 2)), 
-                                    int(round(float(result[1]), 2))])
-                if len(self.tx)<3 and len(self.ty)<3:
-                    self.tx.append(tag_pos[0])
-                    self.ty.append(tag_pos[1])
-                else:
-                    tag_pos[0] = np.median(self.tx, axis=0)
-                    tag_pos[1] = np.median(self.ty, axis=0)
-                    self.tx = []
-                    self.ty = []
-                # self.last_guess = np.array([tag_pos[0]+np.random.randint(-20, 20), 
-                #                             tag_pos[1]+np.random.randint(-20, 20),
-                #                             60])
-                print(tag_pos)
-                # tag_z = int(round(float(result[2]), 2))
-                cv2.circle(map_image, (360-tag_pos[0], 360+tag_pos[1]), 5, (100, 50 , 100), -1)
-                # Clear Buffer
-                self.r0 = []
-                self.r1 = []
-                self.r2 = []
-                cv2.imshow("Map", map_image)
-                cv2.waitKey(1)
-                # Update Initial Guess
-        except KeyboardInterrupt:
-            cv2.destroyAllWindows()
+      # self.get_logger().info("Start_localization")
+      try:
+        # Make sure there are ten dis_data.
+        if len(self.r0)<self.kDataCount or len(self.r1)<self.kDataCount or len(self.r2)<self.kDataCount:
+          # meter to center meter
+          self.r0.append(msgs.x)
+          self.r1.append(msgs.y)
+          self.r2.append(msgs.z)
+          # print(len(self.r0))
+        else:
+          # Average the distance
+          d0 = np.mean(self.r0)
+          d1 = np.mean(self.r1)
+          d2 = np.mean(self.r2)
+          r = np.array([d0, d1, d2]).astype(int)
+          # draw the Anchor Position
+          map_image = np.zeros((720, 720, 3), np.uint8)
+          # map_image[:] = (128, 128, 128)
+          cv2.putText(map_image, '0= '+str(d0), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.putText(map_image, '1= '+str(d1), (100, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.putText(map_image, '2= '+str(d2), (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.circle(map_image, (360+self.anchor0[0], 360+self.anchor0[1]), 2, (255,255,255), 0)
+          cv2.circle(map_image, (360-self.anchor1[0], 360+self.anchor1[1]), 2, (255,255,255), 0)
+          cv2.circle(map_image, (360-self.anchor2[0], 360+self.anchor2[1]), 2, (255,255,255), 0)
+          # # Define Equation
+          # def equations(p):
+            # x, y, z = p
+            # eq1 = (x - self.anchor0[0])**2 + (y - self.anchor0[1])**2 + (z - self.anchor0[2])**2 - d0**2
+            # eq2 = (x - self.anchor1[0])**2 + (y - self.anchor1[1])**2 + (z - self.anchor1[2])**2 - d1**2
+            # eq3 = (x - self.anchor2[0])**2 + (y - self.anchor2[1])**2 + (z - self.anchor2[2])**2 - d2**2
+            # return [eq1, eq2, eq3]
+          # # 初始猜測值
+          # initial_guess = np.array([0, 100, 0])
+          # # 使用 fsolve 求解
+          # result = fsolve(equations, initial_guess, xtol=0.001, maxfev=300)
+          try:
+            #找近似值X
+            self.initial_guess=gradient_descent(self.initial_guess, self.anchor, r)
+          except:
+            pass
+          # Result
+          tag_pos = np.array([int(round(float(self.initial_guess[0]), 2)), 
+                              int(round(float(self.initial_guess[1]), 2))])
+          print(tag_pos)
+          # tag_z = int(round(float(result[2]), 2))
+          cv2.circle(map_image, (360-tag_pos[0], 360+tag_pos[1]), 5, (100, 50 , 100), -1)
+          # Clear Buffer
+          self.r0 = []
+          self.r1 = []
+          self.r2 = []
+          cv2.imshow("Map", map_image)
+          cv2.waitKey(1)
+          # Update Initial Guess
+      except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+
+#定義圓球方程式
+def f(x, center, r):
+  return np.array([
+    (x[0]-center[0][0])**2 + (x[1]-center[0][1])**2 + (x[2]-center[0][2])**2 - r[0]**2,
+    (x[0]-center[1][0])**2 + (x[1]-center[1][1])**2 + (x[2]-center[1][2])**2 - r[1]**2,
+    (x[0]-center[2][0])**2 + (x[1]-center[2][1])**2 + (x[2]-center[2][2])**2 - r[2]**2])
+#定義方程式的梯度
+def grad_f(x, center):
+  return np.array([
+    [2*(x[0]-center[0][0]) , 2*(x[1]-center[0][1]) , 2*(x[2]-center[0][2]) ],
+    [2*(x[0]-center[1][0]) , 2*(x[1]-center[1][1]) , 2*(x[2]-center[1][2]) ],
+    [2*(x[0]-center[2][0]) , 2*(x[1]-center[2][1]) , 2*(x[2]-center[2][2]) ]])
+#梯度下降找近似解
+def gradient_descent(X,center,r):
+  esp=1e-2
+  N=300
+  for i in range(N):
+    # print("iter : {:d}".format(i))
+    fx=f(X,center,r)
+    grad=grad_f(X,center)
+    dx=np.dot(grad.T,fx)
+    X=X-(9e-7)*dx
+    if  np.sqrt(np.sum(dx**2)) < esp:
+        break
+    # print("iter {:d} done dx={:}".format(i, dx))
+    # return [int(X[0]),int(X[1]),int(X[2])]
+  #2D
+  return [int(X[0]),int(X[1]),int(X[2])]
 
 def main(args=None):
     rclpy.init(args=args)
--- uwb_localization/uwb_receiver.py
+++ uwb_localization/uwb_receiver.py
@@ -6,80 +6,123 @@ import numpy as np
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
 
+class custom_kalman1D:
+  def __init__(self):
+    n = 10
+    self.Q = 1e-5  # 過程噪聲協方差
+    self.R = 0.01**2  # 觀測噪聲協方差
+    self.xhat = np.zeros(n)  # 估計值
+    self.xhat = [0,0]
+    self.P = np.zeros(n)  # 估計值的協方差
+    self.P =[0]
+    self.xhatminus = np.zeros(n)  # 預測值
+    self.xhatminus =[0]
+    self.Pminus = np.zeros(n)  # 預測值的協方差
+    self.Pminus =[0]
+    self.K = np.zeros(n)  # 卡爾曼增益
+    self.K =[0]
+    self.xhat[0] = 0.0
+    self.P[0] = 1.0
+
+  def renew_and_getdata(self, raw_data):
+    # 預測
+    # xhatminus[k] = xhat[k-1]
+    self.xhatminus[0]=self.xhat[0]
+    # Pminus[k] = P[k-1] + Q
+    self.Pminus[0]=self.P[0]+self.Q
+    # 更新
+    # K[k] = Pminus[k] / (Pminus[k] + R)
+    self.K[0]=self.Pminus[0]/(self.Pminus[0]+self.R)
+    # xhat[k] = xhatminus[k] + K[k] * (data[k] - xhatminus[k])
+    self.xhat[1]=self.xhatminus[0]+self.K[0]*(raw_data-self.xhatminus[0])
+    # P[k] = (1 - K[k]) * Pminus[k]
+    self.P[0] = (1 - self.K[0]) * self.Pminus[0]
+    self.xhat[0]=self.xhat[1]
+    return self.xhat[0]
+
 
 class UWBReceiver(Node):
 
-    baudRate = 115200
-    distList = []
-    #
-    last_0 = 0.0
-    last_1 = 0.0
-    last_2 = 0.0  
-    # Serial Port
-    serial_uwb0 = serial.Serial('/dev/ttyACM0', baudRate, timeout=1)
-    serial_uwb1 = serial.Serial('/dev/ttyACM1', baudRate, timeout=1)
-    serial_uwb2 = serial.Serial('/dev/ttyACM2', baudRate, timeout=1)
+  a0_kalman = custom_kalman1D()
+  a1_kalman = custom_kalman1D()
+  a2_kalman = custom_kalman1D()
+
+  baudRate = 115200
+  d0 = []
+  d1 = []
+  d2 = []
+  listNum = 3
+  #
+  last_0 = 0.0
+  last_1 = 0.0
+  last_2 = 0.0  
+  # Serial Port
+  serial_uwb0 = serial.Serial('/dev/ttyACM0', baudRate, timeout=1)
+  serial_uwb1 = serial.Serial('/dev/ttyACM1', baudRate, timeout=1)
+  serial_uwb2 = serial.Serial('/dev/ttyACM2', baudRate, timeout=1)
 
     
-    def __init__(self):
-        super().__init__('uwb_init')
-        # Subscriber
-        self.mode_sub_ = self.create_subscription(String, 'robot_mode', self.mode_callback, 10)
-        self.mode_sub_
-        # Publisher
-        self.pose_pub_ = self.create_publisher(Vector3, 'uwb_distance', 10)
-        # Reset Serial
-        self.serial_uwb0.reset_input_buffer()
-        self.serial_uwb1.reset_input_buffer()
-        self.serial_uwb2.reset_input_buffer()
+  def __init__(self):
+    super().__init__('uwb_init')
+    # Subscriber
+    self.mode_sub_ = self.create_subscription(String, 'robot_mode', self.mode_callback, 10)
+    self.mode_sub_
+    # Publisher
+    self.pose_pub_ = self.create_publisher(Vector3, 'uwb_distance', 10)
+    # Reset Serial
+    self.serial_uwb0.reset_input_buffer()
+    self.serial_uwb1.reset_input_buffer()
+    self.serial_uwb2.reset_input_buffer()
 
-    def mode_callback(self, msg):
-        # Receieve Dis Data
-        dis_msgs = Vector3()
-        self.get_logger().info('UWB_dist Start')
-        try:
-            while True:
-                i=0
-                dis_anchor0 = send_and_receive(i, self.serial_uwb0)
-                if dis_anchor0<= 0:
-                    dis_msgs.x = self.last_0
-                else:
-                    dis_msgs.x = dis_anchor0
-                    self.last_0 = dis_anchor0
-                i=1
-                dis_anchor1 = send_and_receive(i, self.serial_uwb1)
-                if dis_anchor1<= 0:
-                    dis_msgs.y = self.last_1
-                else:
-                    dis_msgs.y = dis_anchor1
-                    self.last_1 = dis_anchor1
-                i=2
-                dis_anchor2 = send_and_receive(i, self.serial_uwb2)
-                if dis_anchor2 <= 0:
-                    dis_msgs.z = self.last_2
-                else:
-                    dis_msgs.z = dis_anchor2
-                    self.last_2 = dis_anchor2
-                self.pose_pub_.publish(dis_msgs)
-        except KeyboardInterrupt:
-            self.serial_uwb0.close()
-            self.serial_uwb1.close()
-            self.serial_uwb2.close()
-            print("Program terminated by user.")
+  def mode_callback(self, msg):
+    # Receieve Dis Data
+    dis_msgs = Vector3()
+    self.get_logger().info('UWB_dist Start')
+    try:
+      while True:
+        i=0
+        dis_anchor0 = send_and_receive(i, self.serial_uwb0)*100
+        if dis_anchor0<= 0:
+            dis_msgs.x = self.last_0
+        else:
+            dis_anchor0 = self.a0_kalman.renew_and_getdata(dis_anchor0)
+            dis_msgs.x = dis_anchor0
+            self.last_0 = dis_anchor0
+        i=1
+        dis_anchor1 = send_and_receive(i, self.serial_uwb1)*100
+        if dis_anchor1<= 0:
+            dis_msgs.y = self.last_1
+        else:
+            dis_anchor1 = self.a1_kalman.renew_and_getdata(dis_anchor1)
+            dis_msgs.y = dis_anchor1
+            self.last_1 = dis_anchor1
+        i=2
+        dis_anchor2 = send_and_receive(i, self.serial_uwb2)*100
+        if dis_anchor2 <= 0:
+            dis_msgs.z = self.last_2
+        else:
+            dis_anchor2 = self.a2_kalman.renew_and_getdata(dis_anchor2)
+            dis_msgs.z = dis_anchor2
+            self.last_2 = dis_anchor2
+        self.pose_pub_.publish(dis_msgs)
+    except KeyboardInterrupt:
+      self.serial_uwb0.close()
+      self.serial_uwb1.close()
+      self.serial_uwb2.close()
+      print("Program terminated by user.")
 
 def send_and_receive(uwbNum, serial_receive):
-    try:
+  try:
+    data = serial_receive.readline()
+    dis = data.decode('utf-8').strip()
+    while(dis==''):
         data = serial_receive.readline()
         dis = data.decode('utf-8').strip()
-        while(dis==''):
-            data = serial_receive.readline()
-            dis = data.decode('utf-8').strip()
-            # print('dis=' + str(dis))
-            # print('No Data, Wating:'+str(uwbNum))
-        return float(dis)
-    except:
-        pass
-
+        # print('dis=' + str(dis))
+        # print('No Data, Wating:'+str(uwbNum))
+    return float(dis)
+  except:
+    pass
 
 def main(args=None):
     rclpy.init(args=args)

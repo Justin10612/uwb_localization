c9368572ba5738f65a4bf4bc66508a609bb95ca9
Justin10612 <atom.9031@gmail.com>
2024-05-01 12:12:53 +0800

    Switch to Arduino Trigger

--- uwb_localization/localization.py
+++ uwb_localization/localization.py
@@ -1,16 +1,14 @@
 import rclpy
 from rclpy.node import Node
 import numpy as np
-
 import cv2
-from scipy.optimize import fsolve
 
 from geometry_msgs.msg import Vector3
 
 
 class UwbLocalization(Node):
     # Constants
-    kDataCount = 3
+    kDataCount = 1
     # Dis
     r0 = []
     r1 = []
@@ -20,8 +18,8 @@ class UwbLocalization(Node):
     # Anchor Coordinate
     # [x,y,z]
     anchor = np.array([[0, 0, 22],
-                      [16, 56, 2],
-                      [-16, 56, -2]])
+                      [16, 56, 0],
+                      [-16, 56, 0]])
     anchor0 = np.array([0, 0, 22])
     anchor1 = np.array([16, 56, 2])
     anchor2 = np.array([-16, 56, -2])
@@ -49,32 +47,27 @@ class UwbLocalization(Node):
           # print(len(self.r0))
         else:
           # Average the distance
-          d0 = np.mean(self.r0)
-          d1 = np.mean(self.r1)
-          d2 = np.mean(self.r2)
+          # d0 = np.mean(self.r0)
+          # d1 = np.mean(self.r1)
+          # d2 = np.mean(self.r2)
+          d0 = msgs.x
+          d1 = msgs.y
+          d2 = msgs.z
           r = np.array([d0, d1, d2]).astype(int)
           # draw the Anchor Position
           map_image = np.zeros((720, 720, 3), np.uint8)
           # map_image[:] = (128, 128, 128)
-          cv2.putText(map_image, '0= '+str(d0), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-          cv2.putText(map_image, '1= '+str(d1), (100, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-          cv2.putText(map_image, '2= '+str(d2), (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.putText(map_image, 'A0 = '+str(round(d0, 2)), (360+self.anchor0[0], 300+self.anchor0[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.putText(map_image, 'A1 = '+str(round(d1, 2)), (280-self.anchor1[0], 420+self.anchor1[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+          cv2.putText(map_image, 'A2 = '+str(round(d2, 2)), (360-self.anchor2[0], 420+self.anchor2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
           cv2.circle(map_image, (360+self.anchor0[0], 360+self.anchor0[1]), 2, (255,255,255), 0)
           cv2.circle(map_image, (360-self.anchor1[0], 360+self.anchor1[1]), 2, (255,255,255), 0)
           cv2.circle(map_image, (360-self.anchor2[0], 360+self.anchor2[1]), 2, (255,255,255), 0)
-          # # Define Equation
-          # def equations(p):
-            # x, y, z = p
-            # eq1 = (x - self.anchor0[0])**2 + (y - self.anchor0[1])**2 + (z - self.anchor0[2])**2 - d0**2
-            # eq2 = (x - self.anchor1[0])**2 + (y - self.anchor1[1])**2 + (z - self.anchor1[2])**2 - d1**2
-            # eq3 = (x - self.anchor2[0])**2 + (y - self.anchor2[1])**2 + (z - self.anchor2[2])**2 - d2**2
-            # return [eq1, eq2, eq3]
-          # # 初始猜測值
-          # initial_guess = np.array([0, 100, 0])
-          # # 使用 fsolve 求解
-          # result = fsolve(equations, initial_guess, xtol=0.001, maxfev=300)
+          cv2.circle(map_image, (360+self.anchor0[0], 360+self.anchor0[1]), int(d0), (255,0,10), 0)
+          cv2.circle(map_image, (360-self.anchor1[0], 360+self.anchor1[1]), int(d1), (0,255,10), 0)
+          cv2.circle(map_image, (360-self.anchor2[0], 360+self.anchor2[1]), int(d2), (0,10,255), 0)
+          # Find coordinate
           try:
-            #找近似值X
             self.initial_guess=gradient_descent(self.initial_guess, self.anchor, r)
           except:
             pass
@@ -112,14 +105,13 @@ def gradient_descent(X,center,r):
   N=300
   for i in range(N):
     # print("iter : {:d}".format(i))
-    fx=f(X,center,r)
-    grad=grad_f(X,center)
-    dx=np.dot(grad.T,fx)
-    X=X-(9e-7)*dx
+    fx = f(X,center,r)
+    grad = grad_f(X,center)
+    dx = np.dot(grad.T, fx)
+    X = X-(9e-7)*dx
     if  np.sqrt(np.sum(dx**2)) < esp:
         break
     # print("iter {:d} done dx={:}".format(i, dx))
-    # return [int(X[0]),int(X[1]),int(X[2])]
   #2D
   return [int(X[0]),int(X[1]),int(X[2])]
 
--- uwb_localization/uwb_receiver.py
+++ uwb_localization/uwb_receiver.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 import serial
 import numpy as np
+import time
 
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
@@ -10,7 +11,7 @@ class custom_kalman1D:
   def __init__(self):
     n = 10
     self.Q = 1e-5  # 過程噪聲協方差
-    self.R = 0.01**2  # 觀測噪聲協方差
+    self.R = 0.07**2  # 觀測噪聲協方差
     self.xhat = np.zeros(n)  # 估計值
     self.xhat = [0,0]
     self.P = np.zeros(n)  # 估計值的協方差
@@ -48,6 +49,7 @@ class UWBReceiver(Node):
   a2_kalman = custom_kalman1D()
 
   baudRate = 115200
+  delayTime = 0.005
   d0 = []
   d1 = []
   d2 = []
@@ -57,10 +59,10 @@ class UWBReceiver(Node):
   last_1 = 0.0
   last_2 = 0.0  
   # Serial Port
-  serial_uwb0 = serial.Serial('/dev/ttyACM0', baudRate, timeout=1)
-  serial_uwb1 = serial.Serial('/dev/ttyACM1', baudRate, timeout=1)
-  serial_uwb2 = serial.Serial('/dev/ttyACM2', baudRate, timeout=1)
-
+  serial_uwb0 = serial.Serial('/dev/ttyACM1', baudRate, timeout=1)
+  serial_uwb1 = serial.Serial('/dev/ttyACM2', baudRate, timeout=1)
+  serial_uwb2 = serial.Serial('/dev/ttyACM3', baudRate, timeout=1)
+  serial_arduino = serial.Serial('/dev/ttyACM0', baudRate, timeout=1)
     
   def __init__(self):
     super().__init__('uwb_init')
@@ -73,6 +75,7 @@ class UWBReceiver(Node):
     self.serial_uwb0.reset_input_buffer()
     self.serial_uwb1.reset_input_buffer()
     self.serial_uwb2.reset_input_buffer()
+    self.serial_arduino.reset_output_buffer()
 
   def mode_callback(self, msg):
     # Receieve Dis Data
@@ -80,30 +83,21 @@ class UWBReceiver(Node):
     self.get_logger().info('UWB_dist Start')
     try:
       while True:
-        i=0
-        dis_anchor0 = send_and_receive(i, self.serial_uwb0)*100
-        if dis_anchor0<= 0:
-            dis_msgs.x = self.last_0
-        else:
-            dis_anchor0 = self.a0_kalman.renew_and_getdata(dis_anchor0)
-            dis_msgs.x = dis_anchor0
-            self.last_0 = dis_anchor0
-        i=1
-        dis_anchor1 = send_and_receive(i, self.serial_uwb1)*100
-        if dis_anchor1<= 0:
-            dis_msgs.y = self.last_1
-        else:
-            dis_anchor1 = self.a1_kalman.renew_and_getdata(dis_anchor1)
-            dis_msgs.y = dis_anchor1
-            self.last_1 = dis_anchor1
-        i=2
-        dis_anchor2 = send_and_receive(i, self.serial_uwb2)*100
-        if dis_anchor2 <= 0:
-            dis_msgs.z = self.last_2
-        else:
-            dis_anchor2 = self.a2_kalman.renew_and_getdata(dis_anchor2)
-            dis_msgs.z = dis_anchor2
-            self.last_2 = dis_anchor2
+        r0 = send_and_receive(0, self.serial_uwb0)
+        if r0 is not None:
+            self.serial_arduino.write(b'1\n')
+            time.sleep(self.delayTime)
+        dis_msgs.x = r0
+        r1 = send_and_receive(1, self.serial_uwb1)
+        if r1 is not None:
+            self.serial_arduino.write(b'2\n')
+            time.sleep(self.delayTime)
+        dis_msgs.y = r1
+        r2 = send_and_receive(2, self.serial_uwb2)
+        if r2 is not None:
+            self.serial_arduino.write(b'0\n')
+            time.sleep(self.delayTime)
+        dis_msgs.z = r2
         self.pose_pub_.publish(dis_msgs)
     except KeyboardInterrupt:
       self.serial_uwb0.close()
@@ -112,17 +106,15 @@ class UWBReceiver(Node):
       print("Program terminated by user.")
 
 def send_and_receive(uwbNum, serial_receive):
-  try:
-    data = serial_receive.readline()
-    dis = data.decode('utf-8').strip()
-    while(dis==''):
-        data = serial_receive.readline()
-        dis = data.decode('utf-8').strip()
-        # print('dis=' + str(dis))
-        # print('No Data, Wating:'+str(uwbNum))
-    return float(dis)
-  except:
-    pass
+  data = serial_receive.readline()
+  dis = data.decode('utf-8').strip()
+  # print(str(uwbNum) +'=' + str(dis))
+  while(dis==''):
+      data = serial_receive.readline()
+      dis = data.decode('utf-8').strip()
+      print('Wating:'+str(uwbNum))
+      pass
+  return float(dis)*100 if data else None
 
 def main(args=None):
     rclpy.init(args=args)
